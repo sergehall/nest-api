7bcd61e7ea8f79180b894e779ffc1e090e170746
sergehall <sergioartg@gmail.com>
2022-12-01 22:40:07 -0800

    first commit

---
+++ src/Users/users.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  getUsers() {
+    return [{ id: 1 }, { id: 2 }];
+  }
+  @Get(':id')
+  getUsersById(@Param('id') userId: string) {
+    return [{ id: 1 }, { id: 2 }].find((i) => i.id === +userId);
+  }
+  @Post()
+  createUsers(@Body() inputModel: CreateUserInputModelType) {
+    return [
+      {
+        id: 12,
+        name: inputModel.name,
+        childrenCount: inputModel.childrenCount,
+      },
+    ];
+  }
+}
+
+type CreateUserInputModelType = {
+  name: string;
+  childrenCount: number;
+};
