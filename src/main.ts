891cecb39815d5a7d2d7066775713ad34b3ad0c5
sergehall <sergioartg@gmail.com>
2022-12-03 05:51:00 -0800

    refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { runDb } from './conection/db-connect';
+import * as cookieParser from 'cookie-parser';
 import * as dotenv from 'dotenv';
 dotenv.config();
-import * as cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 5000;
 async function bootstrap() {
