fcd8afc357d1080fa8fbca3096ea2e8d660530c0
sergehall <sergioartg@gmail.com>
2022-12-06 21:31:16 -0800

    add the @HttpCode(204) decorator at a handler

--- src/features/testing  /testing.controller.ts
+++ src/features/testing  /testing.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Delete } from '@nestjs/common';
+import { Controller, Delete, HttpCode } from '@nestjs/common';
 import { TestingService } from './testing.service';
 
 @Controller('testing')
 export class TestingController {
   constructor(protected testingService: TestingService) {}
   @Delete('all-data')
+  @HttpCode(204)
   async delAllData() {
     return await this.testingService.delAllData();
   }
